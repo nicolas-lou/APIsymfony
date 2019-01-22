f08123a21e401f3b9d558ad4ded7d984d56f215f
nicolas-lou <git.nicolas-lou@laposte.net>
2019-01-22 14:04:44 +0100

    ajout route get post sur users et beers

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,10 +2,14 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use AppBundle\Entity\Beer;
 
 
 class DefaultController extends Controller
@@ -27,6 +31,72 @@ class DefaultController extends Controller
      */
     public function beerAction(Request $request)
     {
-        return new JsonResponse([["name"=>"Rabatjoise", "price"=>3.45],["name"=>"Troll", "price"=>4.25]]);
+        $beers = $this->getDoctrine()->getRepository(Beer::class)->findAll();
+        foreach ($beers as $beer){
+            $beersOk[]= [
+                'id'=>$beer->getId(),
+                'name'=>$beer->getName(),
+                'price'=>$beer->getPrice()
+            ];
+        }
+        return new JsonResponse($beersOk);
     }
+
+    /**
+     * @Route("/users", name="users", methods={"GET"})
+     *
+     */
+    public function userAction(Request $request)
+    {
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+        foreach ($users as $user){
+            $usersOk[]= [
+                'id'=>$user->getId(),
+                'name'=>$user->getName(),
+                'firstname'=>$user->getFirstname(),
+                'age'=>$user->getAge()
+
+
+            ];
+        }
+        return new JsonResponse($usersOk);
+    }
+
+    /**
+     * @Route("/addUser", name="addUser", methods={"POST"})
+     *
+     */
+    public function addUserAction(Request $request)
+    {
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $user = new User();
+        $user->setName($request->get('name'));
+        $user->setFirstname($request->get('firstname'));
+        $user->setAge($request->get('age'));
+        $entityManager->persist($user);
+        $entityManager->flush();
+        return new Response("ok");
+
+
+    }
+
+    /**
+     * @Route("/addBeer", name="addBeer", methods={"POST"})
+     *
+     */
+    public function addBeerAction(Request $request)
+    {
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $beer = new Beer();
+        $beer->setName($request->get('name'));
+        $beer->setPrice($request->get('price'));
+        $entityManager->persist($beer);
+        $entityManager->flush();
+        return new Response("ok");
+
+
+    }
+
 }
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstname", type="string", length=255)
+     */
+    private $firstname;
+
+    /**
+     * @var \int
+     *
+     * @ORM\Column(name="age", type="integer")
+     */
+    private $age;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    /**
+     * Set name.
+     *
+     * @param string $name
+     *
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set firstname.
+     *
+     * @param string $firstname
+     *
+     * @return User
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get firstname.
+     *
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set age.
+     *
+     * @param \int $age
+     *
+     * @return User
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    /**
+     * Get age.
+     *
+     * @return \int
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+}
